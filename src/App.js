86f6a0d5a09885768f91b04095df013d6425eb90
vasanth064 <vasanthvdev@gmail.com>
2023-11-14 22:00:39 +0530

    fixed error on merge

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ export const PrivateRoute = ({ children, userType }) => {
 };
 
 function App() {
-  window.addEventListener("mousemove", CssAnimations.customCursorOnMOuseMoveMethod);
+  // window.addEventListener("mousemove", CssAnimations.customCursorOnMOuseMoveMethod);
   return (
     <React.Suspense fallback={<SuspenseLoader />}>
       <Routes>
